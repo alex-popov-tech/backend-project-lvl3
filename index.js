3f54c11ea4b0e16f3da1aaa426bee5f5c9c973e7
Alexander Popov <alex.popov.tech@gmail.com>
2020-06-22 18:20:32 +0300

    review fixes

--- index.js
+++ index.js
@@ -22,41 +22,60 @@ const ensureFile = (filepath, content) => fs.outputFile(filepath, content)
 
 const toSlug = ({ host, pathname }) => _.trim(`${host}${pathname}`, '/').replace(/[/.]/g, '-');
 
-const generatePageFilePath = (outputDir, url) => {
+const toPageFilePath = (outputDir, url) => {
   const filename = `${toSlug(url)}.html`;
   return path.resolve(outputDir, filename);
 };
 
-const generateAssetFilePaths = (outputDir, url, relativeAssetUrls) => {
+const toAssetFilePaths = (outputDir, url, elementInfos) => {
   const dirpath = `${outputDir}/${toSlug(url)}_files`;
-  const assetFilePaths = relativeAssetUrls
-    .map((assetUrl) => _.trim(assetUrl, '/').replace(/\//g, '-'))
+  const assetFilePaths = elementInfos
+    .map(({ attrvalue }) => _.trim(attrvalue, '/').replace(/\//g, '-'))
     .map((filename) => path.resolve(dirpath, filename));
   return assetFilePaths;
 };
 
-const getAssetRelativeUrls = (html) => $('[ src ]', html)
-  .toArray()
-  .filter((it) => it.attribs.src.startsWith('/'))
-  .map((it) => $(it).attr('src'));
+const toElementInfos = (html, baseurl) => {
+  const attributeMapping = {
+    link: 'href',
+    script: 'src',
+    img: 'src',
+    style: 'src',
+  };
+  const toInfo = (element) => {
+    const tagname = element.type;
+    const attrname = attributeMapping[tagname];
+    const attrvalue = element.attribs[attrname];
+    return { tagname, attrname, attrvalue };
+  };
+  return $('link,img,script,style', html)
+    .toArray()
+    .filter((element) => {
+      const { attrvalue } = toInfo(element);
+      return new URL(attrvalue, baseurl.origin).origin === baseurl.origin;
+    })
+    .map((element) => {
+      const { tagname, attrname, attrvalue } = toInfo(element);
+      return {
+        tagname,
+        attrname,
+        attrvalue,
+        absoluteUrl: `${_.trim(baseurl.origin + baseurl.pathname, '/')}${attrvalue}`,
+      };
+    });
+};
 
 const replaceAttributes = (
   initialHtml,
-  attributeName,
-  fromAttributes,
-  toAttributes,
-) => fromAttributes
-  .reduce((html, from, i) => {
-    const to = toAttributes[i];
+  elementInfos,
+  assetFilePaths,
+) => elementInfos
+  .reduce((html, { tagname, attrname, attrvalue }, i) => {
+    const filePath = assetFilePaths[i];
     const $$ = $.load(html);
-    $$(`[ ${attributeName} = '${from}' ]`).attr(attributeName, to);
+    $$(`${tagname}[ ${attrname} = '${attrvalue}' ]`).attr(attrname, filePath);
     return $$.html();
   }, initialHtml);
-
-const toAbsoluteUrls = (relativeUrls, { origin, pathname }) => {
-  const baseUrlPath = pathname === '/' ? '' : pathname;
-  return relativeUrls.map((it) => `${origin}${baseUrlPath}${it}`);
-};
 
 export default (urlString, outputDir) => download(urlString)
   .then((html) => {
@@ -64,24 +83,24 @@ export default (urlString, outputDir) => download(urlString)
     log(`Passed URL: ${url.toJSON()}`);
     log(`Passed output dir: ${outputDir}`);
 
-    const relativeAssetUrls = getAssetRelativeUrls(html);
-    const absoluteAssetUrls = toAbsoluteUrls(relativeAssetUrls, url);
-    log(`Assets found on a page: [${absoluteAssetUrls.join('\n')}]`);
+    const elementInfos = toElementInfos(html, url);
+    log(`Assets found on a page: [${elementInfos.map((it) => JSON.stringify(it)).join('\n')}]`);
 
-    const pageFilePath = generatePageFilePath(outputDir, url);
+    const pageFilePath = toPageFilePath(outputDir, url);
     log(`Html file path: ${pageFilePath}`);
-    const assetFilePaths = generateAssetFilePaths(outputDir, url, relativeAssetUrls);
+    const assetFilePaths = toAssetFilePaths(outputDir, url, elementInfos);
     log(`Assets file paths: [${assetFilePaths.join('\n')}]`);
 
-    const updatedHtml = replaceAttributes(html, 'src', relativeAssetUrls, assetFilePaths);
+    const updatedHtml = replaceAttributes(html, elementInfos, assetFilePaths);
 
     const assetContents = [];
-    const tasks = absoluteAssetUrls.map((assetUrl, i) => ({
-      title: `Dowloading ${assetUrl}...`,
-      task: () => download(assetUrl).then((content) => {
-        assetContents[i] = content;
-      }),
-    }));
+    const tasks = elementInfos.map(({ absoluteUrl }) => absoluteUrl)
+      .map((assetUrl, i) => ({
+        title: `Dowloading ${assetUrl}...`,
+        task: () => download(assetUrl).then((content) => {
+          assetContents[i] = content;
+        }),
+      }));
     const listr = new Listr(tasks, { concurrent: true });
     return listr.run().then(() => ({
       page: { filepath: pageFilePath, content: updatedHtml },
